2f3f5b1b267009c56edd6835f44231ef083fa7fe
d.lohvinov <d.lohvinov@webitel.com>
2021-05-24 15:24:14 +0300

    0.1.58: endpoint-list-getter-api-consumer rest query params encoding [WTEL-1858]

--- src/api-consumers/list-getter/endpoint-list-getter-api-consumer/interfaces/endpoint-get-list-params.ts
+++ src/api-consumers/list-getter/endpoint-list-getter-api-consumer/interfaces/endpoint-get-list-params.ts
@@ -1,12 +1,5 @@
 import BaseGetListParams from '../../base-list-getter-api-consumer/interfaces/base-get-list-params'
 
 export default interface EndpointGetListParams extends BaseGetListParams {
-  parentId?: number | string
-  page?: number
-  size?: number
-  search?: string
   searchQuery?: string
-  sort?: string
-  fields?: string[]
-  ids?: Array<number | string>
 }
