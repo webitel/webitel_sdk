a81d807c7c4d053968239abd2132b66140b13ec5
d.lohvinov <d.lohvinov@webitel.com>
2021-07-09 16:56:48 +0300

    0.1.66: added endpoint-getter-api-consumer query param [WTEL-1946]

--- src/api-consumers/getter/endpoint-getter-api-consumer/endpoint-getter-api-consumer.ts
+++ src/api-consumers/getter/endpoint-getter-api-consumer/endpoint-getter-api-consumer.ts
@@ -1,3 +1,4 @@
+import qs from 'query-string'
 import EndpointApiConsumersBasicParams from '../../_shared/endpoint-api-consumers/endpoint-api-consumers-basic-params'
 import GetterResponse from '../_shared/interfaces/response/getter-response'
 import BaseGetterApiConsumer from '../base-getter-api-consumer/base-getter-api-consumer'
@@ -23,19 +24,30 @@ export default class EndpointGetterApiConsumer extends BaseGetterApiConsumer {
     return this._getItem(params)
   }
 
-  getNestedItem({ parentId, itemId }: BaseGetParams): Promise<GetterResponse> {
+  getNestedItem({
+    parentId,
+    itemId,
+    query,
+  }: BaseGetParams): Promise<GetterResponse> {
     const baseUrl = `${this.baseUrl}/${parentId}/${this.nestedUrl}`
 
-    return this._getItem({ itemId }, baseUrl)
+    return this._getItem({ itemId, query }, baseUrl)
   }
 
   protected async _getItem(
-    { itemId }: BaseGetParams,
+    { itemId, query }: BaseGetParams,
     baseUrl = this.baseUrl
   ): Promise<GetterResponse> {
-    const getUrl = `${baseUrl}/${itemId}`
+    let url = `${baseUrl}/${itemId}`
+    if (query) {
+      url += `?${qs.stringify(query, {
+        skipEmptyString: true,
+        skipNull: true,
+      })}`
+    }
+
     try {
-      let response = await this.instance.get(getUrl)
+      let response = await this.instance.get(url)
       response = this.responseHandler(response)
       if (this.userResponseHandler) {
         response = this.userResponseHandler(response)
