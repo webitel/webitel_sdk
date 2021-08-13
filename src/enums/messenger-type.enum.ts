16ca83261965e2ec9890c22b5a43d3c0c4b0388d
d.lohvinov <d.lohvinov@webitel.com>
2021-08-13 17:00:28 +0300

    0.1.73: added messenger-type.num [WTEL-1652]

---
+++ src/enums/messenger-type.enum.ts
@@ -0,0 +1,9 @@
+enum MessengerType {
+  TELEGRAM = 'telegram',
+  FACEBOOK = 'facebook',
+  WHATSAPP = 'infobip_whatsapp',
+  WEB_CHAT = 'webchat',
+  VIBER = 'viber',
+}
+
+export default MessengerType
