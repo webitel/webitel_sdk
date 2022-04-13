4c01923ec6efe5357ebe09bd362fa2b1ea861b65
igor <navrotskyj@gmail.com>
2022-04-13 12:40:13 +0300

    WTEL-2494

--- src/socket/errors.ts
+++ src/socket/errors.ts
@@ -1,5 +1,10 @@
 export class DeviceNotFoundError extends Error {
+  static id = `device_not_found`
   constructor(msg: string) {
     super(msg)
+  }
+
+  get id() {
+    return DeviceNotFoundError.id
   }
 }
