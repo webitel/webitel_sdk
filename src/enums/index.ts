9f09c55956c09a56b763b12ccf9e37fa77f7e093
Igor Navrotskyj <navrotskyj@gmail.com>
2022-06-01 11:32:42 +0300

    WTEL-2624

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,5 @@
+import MemberStopCause from './queues/member-stop-cause.enum'
 import QueueType from './queues/queue-type.enum'
 
 export { QueueType }
+export { MemberStopCause }
---
+++ src/enums/queues/member-stop-cause.enum.ts
@@ -0,0 +1,11 @@
+enum MemberStopCause {
+  MISSED = 'missed',
+  TIMEOUT = 'timeout',
+  FAILED = 'failed',
+  ABANDONED = 'abandoned',
+  SUCCESS = 'success',
+  CANCEL = 'cancel',
+  EXPIRED = 'expired',
+}
+
+export default MemberStopCause
