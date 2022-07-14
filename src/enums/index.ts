ecb954d29665b3c19467e9fecfd79a617f6df556
dlohvinov <nsblnr@gmail.com>
2022-07-14 11:11:36 +0300

    0.1.141: microsoft language enum [WTEL-2702]

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,5 +1,7 @@
+import MicrosoftLanguage from './cloud-providers/microsoft/microsoft-language.enum'
 import MemberStopCause from './queues/member-stop-cause.enum'
 import QueueType from './queues/queue-type.enum'
 
 export { QueueType }
 export { MemberStopCause }
+export { MicrosoftLanguage }
